8634cab1abc6ffa7373074ddece082b8b4ed30ea
nilufar <ninimamatova0403@gmail.com>
2024-04-02 15:51:52 +0500

    Generics

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -7,20 +7,23 @@ public class Generics {
 
     //TODO: Refactor Method-1
     public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+        List <String>  firstList = new ArrayList <>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
-        secondList.add(firstList);
+        List <String>  secondList = new  ArrayList <>();
+        secondList.add(String.valueOf(firstList));
         return secondList;
     }
 
     //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    public <T> Object genericMethod(Object data) {
+
         return data;
     }
 
     //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+    public  void cloneMethod(List consumer, List producer) {
+        List<List> firstList = new ArrayList<>();
+        firstList.add(consumer);
         consumer.addAll(producer);
     }
 
